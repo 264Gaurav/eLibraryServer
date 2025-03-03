f6aa4ea30fa76274f9f477afa8de16d8f28d6c73
GAURAV SINGH <gauravsingh264209@gmail.com>
2025-03-03 17:45:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,67 @@
-const mongoose=require('mongoose');
-const dotenv=require('dotenv').config();
-const MONGO_URL=process.env.MONGO_URL;
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(MONGO_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+const MONGO_URL = process.env.MONGO_URL;
+if (!MONGO_URL) {
+  console.error('MONGO_URL is not defined in environment variables.');
+  process.exit(1);
+}
 
-const db=mongoose.connection;
+mongoose.connect(MONGO_URL);
 
-db.on('error',(err)=>{
-    console.log('MongoDB connection Error:' , err);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
 });
 
-db.on('connected',()=>{
-    console.log('Database connected');
+db.once('open', () => {
+  console.log('Database connected');
 });
+
+db.on('disconnected', () => {
+  console.log('Database disconnected.');
+});
+
+module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose=require('mongoose');
+// const dotenv=require('dotenv').config();
+// const MONGO_URL=process.env.MONGO_URL;
+
+// mongoose.connect(MONGO_URL,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// );
+
+// const db=mongoose.connection;
+
+// db.on('error',(err)=>{
+//     console.log('MongoDB connection Error:' , err);
+// });
+
+// db.on('connected',()=>{
+//     console.log('Database connected');
+// });
 
 db.on('disconnected', ()=>{
     console.log("Database disconnected.");
